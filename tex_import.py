23507c159c140120a599f9827e87590a8866b5f6
Riccardo Zanotto <rikyzan@gmail.com>
2019-06-02 13:14:07 +0200

    Importate gare italiane

--- tex_import.py
+++ tex_import.py
@@ -9,7 +9,37 @@ def read_source_cese(path):
     with open(path) as infile:
         soup = TexSoup(infile)
         #print(list(soup.enumerate.contents))
-    return [str(c).replace("\item","") for c in soup.enumerate.children]
+    return [str(c).replace("\item","", 1) for c in soup.enumerate.children]
+
+def read_source_archi1(path):
+    print(path)
+    testi = []
+    with open(path) as infile:
+        soup = TexSoup(infile)
+        for it in soup.find_all('item'):
+            if "epsfig" in str(it) or "includegraphics" in str(it):
+                testi.append(None)
+            else:
+                t = str(it)
+                t = t.replace("\item","", 1)
+                t = re.sub(r"\\begin{minipage}\[.\]{.*?}", "", t)
+                t = re.sub(r"\\end{minipage}", "", t)
+                testi.append(t)
+    return testi
+
+def read_source_archi2(path):
+    print(path)
+    testi = []
+    regex = r"^(?:| |  )(?:\\si{)?\\def\\arc..{\\item \\dom{((?:(?!\\risp).)*)}"
+    with open(path) as infile:
+        doc = str(infile.read())
+    for p in re.finditer(regex, doc, re.DOTALL | re.MULTILINE):
+        t = p.groups()[0].strip()
+        t = re.sub(r"\\begin{minipage}\[.\]{.*?}", "", t)
+        t = re.sub(r"\\end{minipage}", "", t)
+        t = re.sub(r"\\begin{tikzpicture}\[scale=(.*?)\]", r"\\begin{tikzpicture}[scale=2]", t)
+        testi.append(t)
+    return testi
 
 def read_source_gas(path):
     print(path)
@@ -30,14 +60,16 @@ def read_source_febb(path):
     res = []
     for p in re.finditer(regex, doc, re.DOTALL):
         testo = p.groups()[0].strip()
-        idx = testo.find("\A")
-        if idx != -1:
-            testo = testo[:idx]
+        testo = testo.replace(r"\A", r"\a")
+        testo = testo.replace(r"\B", r"\b")
+        testo = testo.replace(r"\C", r"\c")
+        testo = testo.replace(r"\D", r"\d")
+        testo = testo.replace(r"\E", r"\e")
         res.append(testo.strip())
     return res
 
 
-def import_gara(root, gara):
+def import_gara(root, gara, dryrun=False):
     session = Session()
     path = os.path.join(root, gara['dir'])
     for root, dirs, files in os.walk(path):
@@ -52,27 +84,37 @@ def import_gara(root, gara):
                 anno += 1900
 
             gara_obj = Gara(nome=gara['nome'], anno=anno, nazione="ITA")
-            session.add(gara_obj)
+            if not dryrun:
+                session.add(gara_obj)
 
             testi = gara['fun'](os.path.join(path, fn))
+            cnt = 0
             for i, t in enumerate(testi):
-                prob = Problema(testo=t, gara=gara_obj, numero=(i+1))
-                session.add(prob)
+                if t is None:
+                    continue
+                cnt += 1
+                if not dryrun:
+                    prob = Problema(testo=t, gara=gara_obj, numero=(i+1), difficolta=gara['diff'])
+                    session.add(prob)
 
-            print(gara_obj, len(testi))
+            print(gara_obj, len(testi), cnt)
 
     session.commit()
+    session.close()
 
 
 
 
 
 
-gare = {"ITA": [
-    {"nome": "Cesenatico", "regex": "ces(\d+).tex", "fun": read_source_cese, "dir": "Cesenatico"},
-    {"nome": "Febbraio", "regex": "testo(\d+).tex", "fun": read_source_febb, "dir": "Febbraio"},
-    {"nome": "Finale a squadre", "regex": "Fgas(\d+)_ITA.tex", "fun": read_source_gas, "dir": "GaS"},
-    {"nome": "Semifinale", "regex": "Agas(\d+)_ITA.tex", "fun": read_source_gas, "dir": "GaS"}]}
+gare = [
+    {"nome": "Cesenatico", "regex": "ces(\d+).tex", "fun": read_source_cese, "dir": "Cesenatico", "diff": 6},
+    {"nome": "Febbraio", "regex": "testo(\d+).tex", "fun": read_source_febb, "dir": "Febbraio", "diff": 3},
+    {"nome": "Finale a squadre", "regex": "Fgas(\d+)_ITA.tex", "fun": read_source_gas, "dir": "GaS", "diff": 5},
+    {"nome": "Semifinale", "regex": "Agas(\d+)_ITA.tex", "fun": read_source_gas, "dir": "GaS", "diff": 5},
+    {"nome": "Archimede", "regex": "(\d+).tex", "fun": read_source_archi1, "dir": "Archimede", "diff": 1},
+    {"nome": "Archimede", "regex": "archimede_triennio_(\d+).tex", "fun": read_source_archi2, "dir": "Archimede", "diff": 1}]
 
 
-#import_gara("gare", gare['ITA'][0])
+for g in gare:
+    import_gara("gare", g)
